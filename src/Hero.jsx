3560b73e597803b0edcb25b6972d3cfdba2b4bf2
theo82 <theoaristi53@gmail.com>
2025-06-28 14:31:16 +0300

    Hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero-container'>
+      <div className='hero-center'>
+        <h1>Manage Any Content</h1>
+        <p>
+          Stapi is the leading open-source headless CMS. It is 100% Javascript
+          and fully customisable
+        </p>
+      </div>
+    </div>
   )
 }
 
