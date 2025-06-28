cc26f0744dab29d0261cfecc28e7f662ff1c3744
theo82 <theoaristi53@gmail.com>
2025-06-28 21:10:57 +0300

    Sidebar JSX

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,8 +1,40 @@
+import { FaTimes } from 'react-icons/fa'
+import sublinks from './data'
+import { useGlobalContext } from './context'
+
 import React from 'react'
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
   return (
-    <div>Sidebar</div>
+    <aside className='sidebar'>
+      <div className='sidebar-container'>
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map((item) => {
+            const { links, page, pageId } = item
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link
+                    return (
+                      <a key={id} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    )
+                  })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </aside>
   )
 }
 
