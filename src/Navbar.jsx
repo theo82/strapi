a0c3e34d458b644cd6c8a0ff21eb8d53d9f7010d
theo82 <theoaristi53@gmail.com>
2025-06-30 15:06:39 +0300

    Hide Submenu (Part 1)

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,9 +4,17 @@ import { useGlobalContext } from './Context'
 import NavLinks from './NavLinks'
 
 const Navbar = () => {
-  const {openSidebar} = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext()
+
+  const handleSubmenu = (e) => {
+    // console.log(e.target)
+    if (!e.target.classList.contains('nav-link')) {
+      setPageId(null)
+    }
+  }
+
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
       <div className='nav-center'>
         <h3 className='logo'>strapi</h3>
         <button className='toggle-btn' onClick={openSidebar}>
